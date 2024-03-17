8de3871679f546a9c3fb8b9ff3564733d46a31f9
chihabMe <chihab.mg.me@gmail.com>
2024-03-17 19:52:39 +0100

    added a repel

--- main.go
+++ main.go
@@ -1,5 +1,18 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"os"
+	"os/user"
 
+	"github.com/chihabMe/mg-lang/repel"
+)
+
+func main() {
+	user, err := user.Current()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("Hello %s\n", user.Username)
+	repel.Start(os.Stdin, os.Stdout)
 }
